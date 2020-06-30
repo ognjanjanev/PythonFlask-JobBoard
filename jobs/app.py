f69ce6d594053c1aa845278b6bd39cc6d2267206
Ognjan Janev <ognjan.janev@gmail.com>
2020-06-30 11:31:48 +0200

    first commit

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 from flask import render_template
 
+PATH = "db/jobs.sqlite"
+
 app = Flask(__name__)
+
+def open_connection():
+    return getattr(o, g)
 
 @app.route('/')
 @app.route('/jobs')
